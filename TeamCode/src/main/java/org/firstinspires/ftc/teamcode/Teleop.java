b8e7e7bc67555f3879e5b3afbee544fb6a80cd77
RohanTD <rtdharia@gmail.com>
2020-08-10 15:30:57 -0500

    Edited teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -7,5 +7,11 @@ public class Teleop extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         DcMotor frontright = hardwareMap.dcMotor.get("front_right");
+
+        waitForStart();
+
+        while (opModeIsActive()){
+            frontright.setPower(0);
+        }
     }
 }
