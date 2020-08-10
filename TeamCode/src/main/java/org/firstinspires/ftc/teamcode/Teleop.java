09ac6d332292022e4ba72a798e19cbbaa8b4e040
RohanTD <rtdharia@gmail.com>
2020-08-10 15:50:33 -0500

    Edited teleop again

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,18 +2,29 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Teleop extends LinearOpMode {
     @Override
     public void runOpMode() {
-        DcMotor frontright = hardwareMap.dcMotor.get("front_right");
-
+        DcMotor frontRight = hardwareMap.dcMotor.get("front_right");
+        frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        frontRight.setPower(0);
         waitForStart();
-
+        frontRight.setPower(0);
         while (opModeIsActive()){
-            frontright.setPower(0);
+            frontRight.setPower(1);
+            delay(500);
             break;
             //this is a test number 2
         }
+    }
+    public void delay(int delayTimeMillis){
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < delayTimeMillis){
+            telemetry.addData("Waiting for ",(System.currentTimeMillis() - startTime) + "/" + (delayTimeMillis) + " milliseconds");
+            telemetry.update();
+        }
+
     }
 }
