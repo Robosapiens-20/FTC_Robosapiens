3da0bd0279a03b69f46742b8a3ada9accc62b3a8
RohanTD <rtdharia@gmail.com>
2020-08-10 14:47:14 -0500

    Edited teleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class Teleop extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotor frontright = hardwareMap.dcMotor.get("front_right");
+    }
+}
